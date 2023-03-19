acdbfd99d24a8fe872cceb89b697354d8b3f3dd6
beatrizpcampos <beatrizcampos300@gmail.com>
2023-03-19 16:35:53 +0000

    finalizado

---
+++ script.js
@@ -0,0 +1,9 @@
+function trocaCor (cor) {
+    let circulo = document.querySelector(".circulo")
+    circulo.style.background = cor
+}
+
+function trocaImg (imagem) {
+    let iphoneImg = document.querySelector(".iphone-img")
+    iphoneImg.src = imagem
+}
